957c34f3e8f7d873f23b711da3c30b0772b2b712
Seth Mwebi <sethmwebi27@gmail.com>
2024-08-18 11:36:05 +0300

    adds homepage and DistanceFinder components

--- src/components/ActivityTracker.jsx
+++ src/components/ActivityTracker.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-
-const ActivityTracker = () => {
-  return <div>ActivityTracker</div>;
-};
+import { useState } from "react";
+import * as Icon from "react-bootstrap-icons";
+function ActivityTracker() {
+  const [actionModal, setActionModal] = useState(false);
+  return (
+    <div className="activity-page">
+      <div
+        className="tab-button"
+        onClick={() => {
+          actionModal ? setActionModal(false) : setActionModal(true);
+        }}
+      >
+        <Icon.Plus />
+      </div>
+      {actionModal ? (
+        <div className="actions">
+          <div className="single-action">
+            <Icon.Bicycle />
+            <p>Cycling</p>
+          </div>
+          <div className="single-action">
+            <Icon.Person />
+            <p>Running</p>
+          </div>
+          <div className="single-action">
+            <Icon.PersonWalking />
+            <p>Walking</p>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
 
 export default ActivityTracker;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,15 @@
+import ActivityTracker from "./ActivityTracker";
+import DashboardTabs from "./DashboardTabs";
+import Navbar from "./Navbar";
+
+function Homepage() {
+  return (
+    <div className="dashboard">
+      <Navbar />
+      <DashboardTabs />
+      <ActivityTracker />
+    </div>
+  );
+}
+
+export default Homepage;
